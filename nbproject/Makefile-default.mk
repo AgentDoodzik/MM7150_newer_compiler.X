df8d35b1512fd3afbe8282dec1a122b93994e168
AgentDoodzik <115586234+AgentDoodzik@users.noreply.github.com>
2023-12-28 18:26:02 +0100

    Add .gitignore

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -107,78 +107,78 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/MM7150.o: MM7150.c  .generated_files/flags/default/cdf8bec6fe4af6aafcab3359f24a86a5d0d0966f .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/MM7150.o: MM7150.c  .generated_files/flags/default/8b41273b08c1b06aa4995911076d28fc1c793dce .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/MM7150.o.d 
 	@${RM} ${OBJECTDIR}/MM7150.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MM7150.o.d" -o ${OBJECTDIR}/MM7150.o MM7150.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MM7150.o.d" -o ${OBJECTDIR}/MM7150.o MM7150.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/InitPIC.o: InitPIC.c  .generated_files/flags/default/e1d34d681ac85aaae49cbd77600a6079b5152e8a .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/InitPIC.o: InitPIC.c  .generated_files/flags/default/c93637538ee3455c59d10bf9105b7d16966b646b .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/InitPIC.o.d 
 	@${RM} ${OBJECTDIR}/InitPIC.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/InitPIC.o.d" -o ${OBJECTDIR}/InitPIC.o InitPIC.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/InitPIC.o.d" -o ${OBJECTDIR}/InitPIC.o InitPIC.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/ceede6b55cca93683cebee9dc22288649ca48286 .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/a55a20ce68b8b2e8ce9d75708124bb94a09d3dec .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/MainInterrupt.o: MainInterrupt.c  .generated_files/flags/default/bfc1e368c03cab9465943003a90ca099a028ae32 .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/MainInterrupt.o: MainInterrupt.c  .generated_files/flags/default/58fd799b87e64ed5da0db6943e5d2507cda30701 .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/MainInterrupt.o.d 
 	@${RM} ${OBJECTDIR}/MainInterrupt.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MainInterrupt.o.d" -o ${OBJECTDIR}/MainInterrupt.o MainInterrupt.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MainInterrupt.o.d" -o ${OBJECTDIR}/MainInterrupt.o MainInterrupt.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/UART_user.o: UART_user.c  .generated_files/flags/default/4a23074870c06480a8d7d2044f764c165acf3ce9 .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/UART_user.o: UART_user.c  .generated_files/flags/default/b466ff6ccf4245ec8b595305aeff7f3d6143e56b .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UART_user.o.d 
 	@${RM} ${OBJECTDIR}/UART_user.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/UART_user.o.d" -o ${OBJECTDIR}/UART_user.o UART_user.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/UART_user.o.d" -o ${OBJECTDIR}/UART_user.o UART_user.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/servo.o: servo.c  .generated_files/flags/default/afcf3942c5036ebbd44301977a80f88c6c3a0726 .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/servo.o: servo.c  .generated_files/flags/default/afa9a739b2ea8e94e127e656c9b172d48d8c782 .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/servo.o.d 
 	@${RM} ${OBJECTDIR}/servo.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/servo.o.d" -o ${OBJECTDIR}/servo.o servo.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/servo.o.d" -o ${OBJECTDIR}/servo.o servo.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
 else
-${OBJECTDIR}/MM7150.o: MM7150.c  .generated_files/flags/default/84a8e38fe23ce6994dba2f391fa3a47542956b77 .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/MM7150.o: MM7150.c  .generated_files/flags/default/c783fe6842e5ecff09759122acd7af5489dce03e .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/MM7150.o.d 
 	@${RM} ${OBJECTDIR}/MM7150.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MM7150.o.d" -o ${OBJECTDIR}/MM7150.o MM7150.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MM7150.o.d" -o ${OBJECTDIR}/MM7150.o MM7150.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/InitPIC.o: InitPIC.c  .generated_files/flags/default/de2c668cea72948d3b3cf8da14f90d5baba3bf2e .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/InitPIC.o: InitPIC.c  .generated_files/flags/default/fdb7eabe43cd3bb3863684658d589bc86f3d11a8 .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/InitPIC.o.d 
 	@${RM} ${OBJECTDIR}/InitPIC.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/InitPIC.o.d" -o ${OBJECTDIR}/InitPIC.o InitPIC.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/InitPIC.o.d" -o ${OBJECTDIR}/InitPIC.o InitPIC.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/e0a46a5977c28dbe644a3ee80d720cdd5dee161a .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/8836e3b7282c01b8ccc8de59042b6608706f6dab .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/MainInterrupt.o: MainInterrupt.c  .generated_files/flags/default/3da0cf903b7b831b6dd5a8a8968f4a28cf37eaaf .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/MainInterrupt.o: MainInterrupt.c  .generated_files/flags/default/b829fa99869f0fe32e0e221e01df7126b0c5ed0c .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/MainInterrupt.o.d 
 	@${RM} ${OBJECTDIR}/MainInterrupt.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MainInterrupt.o.d" -o ${OBJECTDIR}/MainInterrupt.o MainInterrupt.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/MainInterrupt.o.d" -o ${OBJECTDIR}/MainInterrupt.o MainInterrupt.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/UART_user.o: UART_user.c  .generated_files/flags/default/c1dd99c1aeb0e87b7a61388dd7bd3ee11cfd2337 .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/UART_user.o: UART_user.c  .generated_files/flags/default/a8b66d1bfd79846875ae872bc9f3c6f19ee1f8df .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UART_user.o.d 
 	@${RM} ${OBJECTDIR}/UART_user.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/UART_user.o.d" -o ${OBJECTDIR}/UART_user.o UART_user.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/UART_user.o.d" -o ${OBJECTDIR}/UART_user.o UART_user.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/servo.o: servo.c  .generated_files/flags/default/4c31b68bd211fa2ab367ea778cb1f5e053cfde0b .generated_files/flags/default/66e7c6a827ea0b0eafb9971d01f9570a50e9a598
+${OBJECTDIR}/servo.o: servo.c  .generated_files/flags/default/81e05a6c833dbcdaa9d844f04e2aea25d1a05586 .generated_files/flags/default/e71846d3215e1223af1d09b3730e12d0cbee3dbe
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/servo.o.d 
 	@${RM} ${OBJECTDIR}/servo.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/servo.o.d" -o ${OBJECTDIR}/servo.o servo.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)    
+	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/servo.o.d" -o ${OBJECTDIR}/servo.o servo.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
 endif
 
@@ -193,12 +193,12 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${DISTDIR}/MM7150_newer_compiler.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/MM7150_newer_compiler.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x0:0x37F   -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_PK3=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/MM7150_newer_compiler.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x0:0x37F   -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_PK3=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
 	
 else
 ${DISTDIR}/MM7150_newer_compiler.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/MM7150_newer_compiler.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/MM7150_newer_compiler.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
 	${MP_CC_DIR}\\xc32-bin2hex ${DISTDIR}/MM7150_newer_compiler.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
 endif
 
